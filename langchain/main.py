65d3ccba95b7c39371d6dc536bb11d5b4b9a377d
Iron Tony <lifegamecom1@gmail.com>
2023-05-14 00:29:40 -0400

    Added mock Embeddings and LLM

--- langchain/main.py
+++ langchain/main.py
@@ -3,15 +3,20 @@
 # https://python.langchain.com/en/latest/modules/indexes/getting_started.html
 # https://python.langchain.com/en/latest/use_cases/question_answering/semantic-search-over-chat.html
 
-from typing import Any, List
+from typing import Any, List, Optional
 
 import pandas as pd
 from dotenv import load_dotenv
+from langchain.base_language import BaseLanguageModel
+from langchain.callbacks.manager import Callbacks
 from langchain.chains import RetrievalQA
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.base import Embeddings
 from langchain.llms import OpenAI
+from langchain.schema import Generation
+from langchain.schema import PromptValue, LLMResult
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
@@ -54,17 +59,45 @@ class DataFrameLoader(BaseLoader):
         return result
 
 
+class MyLanguageModel(BaseLanguageModel):
+    def generate_prompt(self, prompts: List[PromptValue], stop: Optional[List[str]] = None,
+                        callbacks: Callbacks = None) -> LLMResult:
+        generation = Generation(text="Hello World!")
+        result = LLMResult(generations=[[generation]])
+        return result
+
+    async def agenerate_prompt(self, prompts: List[PromptValue], stop: Optional[List[str]] = None,
+                               callbacks: Callbacks = None) -> LLMResult:
+        pass  # "whatever dude"
+
+
+# NOTE: the OpenAIEmbeddings embeddings have the dimensionality of 1536
+class MyEmbeddings(Embeddings):
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return [[1.0] * 1536, [2.0] * 1536]
+
+    def embed_query(self, text: str) -> List[float]:
+        return [1.0] * 1536
+
+
 df = pd.read_pickle('../data/course_info.pkl')
 
 loader = DataFrameLoader(df, ["title_en", "overview_objectives",
                               "overview_learning_outcomes", "overview_description.en"])
+# NOTE: if the chunk size is too small, then there will be no way to tell which course this information corresponds to
+# Ideally, every chunk should contain information about a single course OR every chunk should have some essential
+# information about a course
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-embeddings = OpenAIEmbeddings()
-llm = OpenAI(temperature=0.9)
+
+# embeddings = OpenAIEmbeddings()
+embeddings = MyEmbeddings()
+# llm = OpenAI(temperature=0.9)
+llm = MyLanguageModel()
 
 documents = loader.load()
 texts = text_splitter.split_documents(documents)
-db = Chroma.from_documents(texts, embeddings, persist_directory="../data/chroma")
+# db = Chroma.from_documents(texts, embeddings, persist_directory="../data/chroma")
+db = Chroma(persist_directory="../data/chroma", embedding_function=embeddings)  # load from disk
 retriever = db.as_retriever()
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
